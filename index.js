1449ac6c95e08e6d7cae59775a0722798f0d1833
Zettat123 <zettat123@gmail.com>
2019-03-30 11:03:17 +0800

    Save to file

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const axios = require('axios')
+const fs = require('fs')
 
-const url = 'http://www.baidu.com'
-const startTime = new Date().getTime()
+const urls = {
+  baidu: 'http://www.baidu.com',
+  zhihu: 'http://www.zhihu.com',
+  taobao: 'http://www.taobao.com',
+  bilibili: 'http://www.bilibili.com',
+}
 
-axios.get(url).then(function(response) {
-  const endTime = new Date().getTime()
+const urlKeys = Object.keys(urls)
 
-  console.log('Loading time is %d\n', endTime - startTime)
+urlKeys.map(index => {
+  const startTime = new Date().getTime()
+  axios.get(urls[index]).then(response => {
+    const endTime = new Date().getTime()
+    console.log('%s Loading time is %d\n', index, endTime - startTime)
+    const fd = fs.openSync(`./foo-${index}.html`, 'w')
+
+    fs.writeFileSync(fd, response.data)
+  })
 })
