5f060f867b005e0335e54fdcc1f20a62ad7dd490
zettat123 <zettat123@gmail.com>
2019-04-02 21:10:52 +0800

    Remove other websites

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
 const axios = require('axios')
 const fs = require('fs')
 
-const urls = {
-  baidu: 'http://www.baidu.com',
-  zhihu: 'http://www.zhihu.com',
-  taobao: 'http://www.taobao.com',
-  bilibili: 'http://www.bilibili.com',
-}
-
-const urlKeys = Object.keys(urls)
-
-urlKeys.map(index => {
-  const startTime = new Date().getTime()
-  axios.get(urls[index]).then(response => {
-    const endTime = new Date().getTime()
-    console.log('%s Loading time is %d\n', index, endTime - startTime)
-    const fd = fs.openSync(`./foo-${index}.html`, 'w')
-
-    fs.writeFileSync(fd, response.data)
-  })
+const url = 'https://www.baidu.com'
+const startTime = new Date().getTime()
+axios.get(url).then(response => {
+  const endTime = new Date().getTime()
+  console.log('Loading time is %d\n', endTime - startTime)
 })
