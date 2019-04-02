acc47dbd97d503c37f1bdd19edfd9123b1e32f96
zettat123 <zettat123@gmail.com>
2019-04-02 21:13:24 +0800

    Save to file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const axios = require('axios')
 const fs = require('fs')
 
-const url = 'https://www.baidu.com'
+const url = 'http://www.baidu.com'
 const startTime = new Date().getTime()
 axios.get(url).then(response => {
   const endTime = new Date().getTime()
   console.log('Loading time is %d\n', endTime - startTime)
+
+  const fd = fs.openSync('get-baidu.html', 'w')
+  fs.writeFileSync(fd, response.data)
 })
